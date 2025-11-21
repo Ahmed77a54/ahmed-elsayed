d3d60ba338eb49ca880b3084b02f6f91d6f9dbbd
Ahmed77a54 <ahmed.elsayed.100000@gmail.com>
2025-11-21 20:34:34 +0200

    Create file2.c

---
+++ file2.c
@@ -0,0 +1,10 @@
+/* Second module: Main program entry point */
+
+// External function declaration
+void display_greeting(void);
+
+int main(void) {
+    // Invoke function from external module
+    display_greeting();
+    return 0;
+}
