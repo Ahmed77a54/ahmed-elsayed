7199659c12f64ff40a2eb4798c7e0ff2e0671897
Ahmed77a54 <ahmed.elsayed.100000@gmail.com>
2025-11-21 20:35:05 +0200

    Create file1.c

---
+++ file1.c
@@ -0,0 +1,7 @@
+/* First module: Function implementation */
+#include <stdio.h>
+
+// Function that displays a greeting message
+void display_greeting(void) {
+    printf("Greetings from the first module!\n");
+}
